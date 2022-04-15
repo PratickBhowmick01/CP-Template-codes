8960f712b1e5ca18592ece8abc2bf3a6aa730397
Pratick Bhowmick <pratickbhowmick01@email.com>
2022-04-15 20:53:23 +0600

    First commit

---
+++ testSetup.py
@@ -0,0 +1,12 @@
+import math, os, sys
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r")
+    sys.stdout = open("output.txt","w")
+ 
+import sys
+input = sys.stdin.readline
+
+#Main code here
+
+sys.stdout.close() 
+sys.stdin.close() 
